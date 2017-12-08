26bde062b97153a4a1200ce064d375a9c9fbc0e1
lmccorkle1 <34359986+lmccorkle1@users.noreply.github.com>
2017-12-07 19:02:34 -0800

    Create imgproc.h

---
+++ imgproc.h
@@ -0,0 +1,18 @@
+//
+//  header.h
+//  finalProjectCS1a
+//
+//  Created by Landon McCorkle on 11/30/17.
+//  Copyright © 2017 Landon McCorkle. All rights reserved.
+//
+
+#ifndef header_h
+#define header_h
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+#endif /* header_h */
